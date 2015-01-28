7c0f7426650b9d3b8a6cdc1256ba97a90a597cac
John Tan <tantai.john@gmail.com>
2015-01-27 22:39:37 -0800

    Union Find init

---
+++ src/UnionFindTest.java
@@ -0,0 +1,44 @@
+class UnionFind {
+    private int[] ids;
+    private int[] sizes;
+
+    UnionFind(int N){
+        ids = new int[N];
+        sizes = new int[N];
+        for(int i=0;i<N;i++) {
+            ids[i] = i;
+            sizes[i] = i;
+        }
+    }
+
+    public int[] getIds(){
+        return ids;
+    }
+
+    private int root(int i){
+        while(i != ids[i]){
+            i = ids[i];
+        }
+        return i;
+    }
+
+    private boolean connected(int origin, int dest){
+        return root(origin) == root(dest);
+    }
+
+    private void union(int origin, int dest){
+        int originRoot = root(origin);
+        int destRoot = root(dest);
+        ids[root(origin)] = root(dest);
+    }
+}
+
+public class UnionFindTest {
+    public static void main(String args[]){
+        UnionFind UF = new UnionFind(10);
+        int [] ids = UF.getIds();
+        for(int i=0;i<ids.length;i++){
+            System.out.println(ids[i]);
+        }
+    }
+}
