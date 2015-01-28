14be8b13204e791601b0819c495af302b6be863d
John Tan <tantai.john@gmail.com>
2015-01-28 13:17:09 -0800

    Initialized percolationstats

---
+++ src/PercolationStats.java
@@ -0,0 +1,29 @@
+public class PercolationStats {
+    private Percolation percolation;
+    private int gridSize;
+    private int numTests;
+    PercolationStats(int N, int T) {
+        gridSize = N;
+        numTests = T;
+    }
+
+    public double mean()
+    {
+
+    }
+
+    public double stddev()
+    {
+
+    }
+
+    public confidenceLo()
+    {
+
+    }
+
+    public double confidenceHi()
+    {
+
+    }
+}
