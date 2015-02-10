21773a35a96d6cb4d3de754f5cc44eefb9ed15a4
John Tan <tantai.john@gmail.com>
2015-02-10 14:28:38 -0800

    Fixed bugs

--- src/week3/Brute.java
+++ src/week3/Brute.java
@@ -15,39 +15,39 @@ public class Brute {
             int y = file.readInt();
             points[i] = new Point(x, y);
 
-            for(int i = 0;i<points.length;i++){
-                points[i].draw();
-                for(int j=0;j<points.length;j++){
-                    for(int k=0;k<points.length;k++){
-                        for(int z=0;z<points.length;z++){
-                            Point current = points[i];
+            i++;
+        }
 
-                            Point[] result = new Point[4];
+        for(int p = 0;i<points.length;i++){
+            points[p].draw();
+            for(int j=0;j<points.length;j++){
+                for(int k=0;k<points.length;k++){
+                    for(int z=0;z<points.length;z++){
+                        Point current = points[p];
 
-                            double slope1 = current.slopeTo(points[j]);
-                            double slope2 = current.slopeTo(points[k]);
-                            double slope3 = current.slopeTo(points[z]);
+                        Point[] result = new Point[4];
+
+                        double slope1 = current.slopeTo(points[j]);
+                        double slope2 = current.slopeTo(points[k]);
+                        double slope3 = current.slopeTo(points[z]);
 
 
-                            if(slope1 == slope2 && slope1 == slope3){
-                                result[0] = current;
-                                result[1] = points[j];
-                                result[2] = points[k];
-                                result[3] = points[z];
+                        if(slope1 == slope2 && slope1 == slope3){
+                            result[0] = current;
+                            result[1] = points[j];
+                            result[2] = points[k];
+                            result[3] = points[z];
 
-                                Arrays.sort(result);
-                                result[0].drawTo(result[1]);
-                                result[1].drawTo(result[2]);
-                                result[2].drawTo(result[3]);
-                                
-                                System.out.println(String.format("(%d, %d) -> (%d, %d) -> (%d, %d) -> (%d, %d)", result[0].x, result[0].y, result[1].x, result[1].y, result[2].x, result[2].y, result[3].x, result[3].y));
-                            }
+                            Arrays.sort(result);
+                            result[0].drawTo(result[1]);
+                            result[1].drawTo(result[2]);
+                            result[2].drawTo(result[3]);
+
+                            System.out.println(String.format("(%d, %d) -> (%d, %d) -> (%d, %d) -> (%d, %d)", result[0].x, result[0].y, result[1].x, result[1].y, result[2].x, result[2].y, result[3].x, result[3].y));
                         }
                     }
                 }
             }
-
-            i++;
         }
     }
 }
