ba20ec217152455aa721fb7ccf32a0c2010d6612
John Tan <tantai.john@gmail.com>
2015-02-10 11:47:48 -0800

    Added week 3

---
+++ src/week2/Subset.java
@@ -0,0 +1,12 @@
+public class Subset {
+    public static void main(String[] args)
+    {
+        RandomizedQueue testQueue = new RandomizedQueue();
+        while(!StdIn.isEmpty()){
+            testQueue.enqueue(StdIn.readString());
+        }
+        for(int i=0;i<Integer.parseInt(args[0]);i++){
+            System.out.println(testQueue.dequeue());
+        }
+    }
+}
