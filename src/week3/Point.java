567d390cbd40d5e02e5816b479f999441d05c618
John Tan <tantai.john@gmail.com>
2015-02-10 12:04:21 -0800

    Completed Point methods

--- src/week3/Point.java
+++ src/week3/Point.java
@@ -15,10 +15,17 @@ import java.util.Comparator;
 public class Point implements Comparable<Point> {
 
     // compare points by slope
-    public final Comparator<Point> SLOPE_ORDER;       // YOUR DEFINITION HERE
+    public final Comparator<Point> SLOPE_ORDER = new SlopeOrder();       // YOUR DEFINITION HERE
 
     private final int x;                              // x coordinate
     private final int y;                              // y coordinate
+
+    public static class SlopeOrder implements Comparator<Point> {
+        public int compare(Point v, Point w)
+        {
+            return (int)v.slopeTo(w);
+        }
+    }
 
     // create the point (x, y)
     public Point(int x, int y) {
@@ -41,13 +48,25 @@ public class Point implements Comparable<Point> {
 
     // slope between this point and that point
     public double slopeTo(Point that) {
-        /* YOUR CODE HERE */
+        return (that.x - this.x) / (that.y - this.y);
     }
 
     // is this point lexicographically smaller than that one?
     // comparing y-coordinates and breaking ties by x-coordinates
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
+        if(this.y > that.y){
+            return 1;
+        }else if(this.y == that.y){
+            if(this.x > that.x){
+                return 1;
+            }else if(this.x == that.x){
+                return 0;
+            }else{
+                return -1;
+            }
+        }else {
+            return -1;
+        }
     }
 
     // return string representation of this point
@@ -58,6 +77,11 @@ public class Point implements Comparable<Point> {
 
     // unit test
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
+        Point test = new Point(1, 1);
+        Point test2 = new Point(2, 2);
+
+        double compare = test.slopeTo(test2);
+
+        System.out.print(compare);
     }
 }
