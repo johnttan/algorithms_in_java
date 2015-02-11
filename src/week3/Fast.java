04c91e2ab54085aad6db280125f4b1e9303b7c73
John Tan <tantai.john@gmail.com>
2015-02-10 17:05:03 -0800

    Completed fast

--- src/week3/Fast.java
+++ src/week3/Fast.java
@@ -1,5 +1,5 @@
-import java.util.Arrays;
 
+import java.util.Arrays;
 public class Fast {
     public static void main (String[] args) {
         StdDraw.setXscale(0, 32768);
@@ -15,21 +15,29 @@ public class Fast {
             int y = file.readInt();
             points[i] = new Point(x, y);
 
-            Arrays.sort(points, Point.SLOPE_ORDER);
-
+            ArrayList<Point> current;
+            i++;
+        }
+        Point[] pointsCopy = Arrays.copyOf(points, points.length);
+        for(int q=0;q<points.length;q++){
+            Arrays.sort(pointsCopy, points[q].SLOPE_ORDER);
             int j = 0;
 
-            ArrayList<Point> current;
-            for(int q=0;q<points.length;q++){
-                while(j < points.length - 4){
-                    if(points[q].slopeTo(points[j]))
-
-                    j++;
+            while(j < points.length - 4){
+                if(points[q].compareTo(pointsCopy[j]) == 0 && points[q].compareTo(pointsCopy[j+1]) == 0 && points[q].compareTo(pointsCopy[j+2]) == 0){
+                    points[j].drawTo(pointsCopy[j+1]);
+                    pointsCopy[j+1].drawTo(pointsCopy[j+2]);
+                    pointsCopy[j+2].drawTo(pointsCopy[j+3]);
+                    Point[] result = new Point[4];
+                    result[0] = points[q];
+                    result[1] = points[j];
+                    result[2] = points[j+1];
+                    result[3] = points[j+2];
+                    System.out.println(String.format("%s -> %s -> %s -> %s", result[0].toString(), result[1].toString(), result[2].toString(), result[3].toString()));
                 }
+
+                j++;
             }
-
-
-            i++;
         }
     }
 }
