bc5045977d44898790c6dae4aca8344e61e89dbb
John Tan <tantai.john@gmail.com>
2015-02-11 00:24:09 -0800

    Fixed some bugs

--- src/week3/Point.java
+++ src/week3/Point.java
@@ -14,19 +14,27 @@ import java.util.Comparator;
 public class Point implements Comparable<Point> {
 
     // compare points by slope
-    public final Comparator<Point> SLOPE_ORDER = new SlopeOrder(Point.this);       // YOUR DEFINITION HERE
+    public final Comparator<Point> SLOPE_ORDER = new SlopeOrder();       // YOUR DEFINITION HERE
 
     private final int x;                              // x coordinate
     private final int y;                              // y coordinate
 
-    private static class SlopeOrder implements Comparator<Point> {
+    private class SlopeOrder implements Comparator<Point> {
         private Point currentPoint;
-        SlopeOrder (Point point) {
-            currentPoint = point;
+        SlopeOrder () {
+
         }
         public int compare(Point v, Point w)
         {
-            return (int)v.slopeTo(currentPoint) - (int)w.slopeTo(currentPoint);
+            Point currentPoint = Point.this;
+            int result = (int) (v.slopeTo(currentPoint) - w.slopeTo(currentPoint));
+            if(result < 0){
+                return -1;
+            }else if(result > 0){
+                return 1;
+            }else{
+                return 0;
+            }
         }
     }
 
@@ -56,6 +64,13 @@ public class Point implements Comparable<Point> {
         }
         if(that.x == this.x){
             return Double.POSITIVE_INFINITY;
+        }
+        if(that.y == this.y){
+            if(that.x > this.x){
+                return -0;
+            }else{
+                return +0;
+            }
         }
         double x = that.x;
         double y = that.y;
@@ -91,6 +106,7 @@ public class Point implements Comparable<Point> {
 
     // unit test
     public static void main(String[] args) {
+        Point test3;
         Point test = new Point(1, 5);
         Point test2 = new Point(1, 2);
         double compare = test.slopeTo(test2);
@@ -125,5 +141,19 @@ public class Point implements Comparable<Point> {
         test2 = new Point(2, 2);
         compare = test.slopeTo(test2);
         assert compare == 1 : "should be 1";
+
+        test = new Point(1, 1);
+        test2 = new Point(2, 2);
+        test3 = new Point(3, 3);
+        compare = test.SLOPE_ORDER.compare(test2, test3);
+        assert compare == 0.0 : "should be 0.0";
+
+        Point p = new Point(492, 112);
+        Point q = new Point(257, 470);
+        Point r = new Point(492, 310);
+        int compare1 = p.SLOPE_ORDER.compare(q, r);
+        int compare2 = p.SLOPE_ORDER.compare(r, q);
+        System.out.println(compare1);
+        System.out.println(compare2);
     }
 }
