9dd507b6d5d1027ed0661898b4986470736cd0c1
John Tan <tantai.john@gmail.com>
2015-02-16 16:37:04 -0800

    Implemented more methods

--- src/week4/Board.java
+++ src/week4/Board.java
@@ -1,23 +1,27 @@
 public class Board {
     private int[][] board;
-
-    public Board(int[][] blocks)
+    private int N;
+    public Board(int[][] blocks) throws NullPointerException
     {
+        if(blocks == null){
+            throw new NullPointerException();
+        }
         board = blocks;
+        N = board.length;
     }
 
     public int dimension()
     {
-
+        return N;
     }
 
     public int hamming()
     {
         int count = 0;
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                int realX = board[i][j] % board.length;
-                int realY = board[i][j] / board.length;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                int realX = board[i][j] % N;
+                int realY = board[i][j] / N;
                 if(i != realX || j != realY){
                     count ++;
                 }
@@ -29,10 +33,10 @@ public class Board {
     public int manhattan()
     {
         int count = 0;
-        for(int i=0;i<board.length;i++) {
-            for (int j = 0; j < board.length; j++) {
-                int realX = board[i][j] % board.length;
-                int realY = board[i][j] / board.length;
+        for(int i=0;i<N;i++) {
+            for (int j = 0; j < N; j++) {
+                int realX = board[i][j] % N;
+                int realY = board[i][j] / N;
                 count += Math.abs(realX - i) + Math.abs(realY - j);
             }
         }
@@ -41,7 +45,7 @@ public class Board {
 
     public boolean isGoal()
     {
-
+        return hamming() == 0;
     }
 
     public Board twin()
@@ -51,21 +55,33 @@ public class Board {
 
     public boolean equals(Object y)
     {
-
+        if(y == this){
+            return true;
+        }
+        if(y == null){
+            return false;
+        }
+        if(x.getClass() != this.getClass()){
+            return false;
+        }
+        Board that = (Board) y;
+        return that.toString().equals(toString());
     }
 
-    public String toString()
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", board[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args)
     {
 
     }
-
-    public static void(String[] args)
-    {
-
-    }
-
-
-
-
-
 }
