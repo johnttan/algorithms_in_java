3e429548813415f9492bd430b0a93e6a5a5e8dab
John Tan <tantai.john@gmail.com>
2015-02-16 19:57:06 -0800

    Added iterable

--- src/week4/Solver.java
+++ src/week4/Solver.java
@@ -1,8 +1,16 @@
+import java.util.ArrayDeque;
+
 public class Solver {
-    private MinPQ<SearchNode> queue;
+    private MinPQ<SearchNode> queueOne;
+    private MinPQ<SearchNode> queueTwo;
 
     private SearchNode searchOne;
     private SearchNode searchTwo;
+
+    private int searchOneCount;
+    private int searchTwoCount;
+
+    private ArrayDeque<Board> solutionQueue;
 
     private class SearchNode implements Comparable<SearchNode> {
         private Board grid;
@@ -13,6 +21,7 @@ public class Solver {
         {
             board = grid;
             priority = grid.hamming() + numMoves;
+            previous = null;
         }
 
         public void assignPrevious(SearchNode prev)
@@ -49,21 +58,42 @@ public class Solver {
 
     public Solver(Board initial)
     {
+        queueOne = new MinPQ<SearchNode>();
+        queueTwo = new MinPQ<SearchNode>();
 
+        searchOne = new SearchNode(initial, 0);
+        searchTwo = new SearchNode(initial.twin(), 0);
     }
 
     public boolean isSolvable()
     {
-
+        return searchOne.getBoard().hamming() == 0;
     }
 
     public int moves()
     {
-
+        if(!isSolvable()){
+            return -1;
+        }else{
+            return searchOneCount;
+        }
     }
 
     public Iterable<Board> solution()
     {
+        if(!isSolvable()){
+            return null;
+        }else{
+            solutionQueue = new ArrayDeque<Board>();
+            SearchNode current = searchOne;
+
+            while(current != null){
+                solutionQueue.add(current.getBoard());
+                current = current.getPrevious();
+            }
+            return solutionQueue;
+        }
+
 
     }
 
