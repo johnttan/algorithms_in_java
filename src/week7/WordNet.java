96ac6a4bc1b9544cebd50261a0ead334212964f1
John Tan <tantai.john@gmail.com>
2015-03-25 11:51:09 -0700

    implemented distance

--- src/week7/WordNet.java
+++ src/week7/WordNet.java
@@ -14,43 +14,32 @@ import java.lang.Integer;
 
 
 public class WordNet {
-    private class SynSet {
-        private String id;
-        private ArrayList<SynSet> edges;
-        public SynSet (String id){
-            id = id;
-            edges = new ArrayList<SynSet>();
-        }
-    }
-    private TreeMap<String, SynSet> nounIndex;
-    private TreeMap<String, SynSet> graphIndex;
+    private TreeMap<String, Integer> nounIndex;
     private Digraph graph;
+    private int countV;
+    
     public WordNet (String synsets, String hypernyms) {
-        nounIndex = new TreeMap<String, SynSet>();
-        graphIndex = new <String, SynSet>TreeMap();
+        nounIndex = new TreeMap<String, Integer>();
         
         In scanSyn;
         In scanHyper;
         scanSyn = new In(synsets);
         scanHyper = new In(hypernyms);
-        int countV = 0;
+        countV = 0;
         while(scanSyn.hasNextLine()){
             String current = scanSyn.readLine();
             String[] splitCurrent = current.split(",");
             String[] nouns = splitCurrent[1].split(" ");
-            SynSet syn = new SynSet(splitCurrent[0]);
-//            Put in Graph
-            graphIndex.put(splitCurrent[0], syn);
             countV ++;
 //            Put in index that allows logn lookup of associated synsets via noun keys
             for(String noun : nouns) {
-                nounIndex.put(noun, syn);
+                nounIndex.put(noun, Integer.parseInt(splitCurrent[0]));
             }
         }
         graph = new Digraph(countV);
         
         while (scanHyper.hasNextLine()) {
-            String current = scanSyn.readLine();
+            String current = scanHyper.readLine();
             String[] hypers = current.split(",");
 //            Add associated hypernyms
             for(int i=1;i<hypers.length;i++){
@@ -70,7 +59,31 @@ public class WordNet {
     }
     
     public int distance(String nounA, String nounB) {
+//        BFS over graph, keep lookup table of (id, distance) to keep track of min distance from nounA
+        int[] distanceTable = new int[countV];
+        boolean[] visitedTable = new boolean[countV];
+        int startV = nounIndex.get(nounA);
+        int endV = nounIndex.get(nounB);
         
+        distanceTable[startV] = 0;
+        
+        Queue<Integer> bfsQueue = new Queue<Integer>();
+        bfsQueue.enqueue(startV);
+        
+        while(!bfsQueue.isEmpty()){
+            Integer currentV = bfsQueue.dequeue();
+            for(Integer v : graph.adj(currentV)){
+                if(!visitedTable[v]){
+                    bfsQueue.enqueue(v);
+                    distanceTable[v] = distanceTable[currentV] + 1;
+                }
+                if(v == endV){
+                    return distanceTable[endV];
+                }
+            }
+            visitedTable[currentV] = true;
+        }        
+        return distanceTable[endV];
     }
     
     public String sap(String nounA, String nounB) {
@@ -78,7 +91,7 @@ public class WordNet {
     }
     
     public static void main (String[] args) {
-        WordNet test = new WordNet("test", "test");
+        WordNet test = new WordNet("src/week7/wordnet/synsets.txt", "src/week7/wordnet/hypernyms.txt");
         System.out.println(test);
     }
 }
