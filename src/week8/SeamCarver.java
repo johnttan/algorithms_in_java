85c2c7fbdca542396a5da3f745ac35a40484ecac
John Tan <tantai.john@gmail.com>
2015-03-30 21:24:13 -0700

    fixed vertical transposition

--- src/week8/SeamCarver.java
+++ src/week8/SeamCarver.java
@@ -12,7 +12,10 @@ import java.awt.Color;
  */
 public class SeamCarver {
     private Picture pic;
+    private Picture tempPic;
     private double[][] energyGrid;
+    private double[][] tempEnergyGrid;
+    
     private boolean rightSide;
             
     private double getEnergy(Picture picture, int x, int y){
@@ -46,6 +49,8 @@ public class SeamCarver {
         } else {
             double xSum = Math.pow(right.getBlue() - left.getBlue(), 2) + Math.pow(right.getGreen() - left.getGreen(), 2) + Math.pow(left.getRed() - left.getRed(), 2);
             double ySum = Math.pow(bottom.getBlue() - top.getBlue(), 2) + Math.pow(bottom.getGreen() - top.getGreen(), 2) + Math.pow(bottom.getRed() - top.getRed(), 2);
+            System.out.println("ENERGIES ");
+            System.out.print(xSum + ySum);
             return xSum + ySum;
         }
     }
@@ -61,37 +66,41 @@ public class SeamCarver {
         return grid;
     }
     
-    private void transposeImage(){
+    private Picture transposeImage(Picture pic){
         Picture picture = new Picture(pic.height(), pic.width());
         if(rightSide){
             for(int x=0;x<pic.width();x++){
                 for(int y=0;y<pic.height();y++){
-                    picture.set(pic.height() -1 - x, x, pic.get(x, y));
+                    picture.set(pic.height() -1 - y, x, pic.get(x, y));
                 }
             }
         }else{
             for(int x=0;x<pic.width();x++){
                 for(int y=0;y<pic.height();y++){
-                    picture.set(y, pic.width()-1-x, pic.get(x, y));
+                    picture.set(y, picture.width()-1-x, pic.get(x, y));
                 }
             }
         }
-        pic = picture;
+        tempEnergyGrid = generateEnergy(picture);
+        return picture;
     }
     private int nodeID(int x, int y) {
-        return x + y * pic.width();
+        return x + y * tempPic.width();
     }
 
     private int[] idToCoord(int id) {
         int[] coord = new int[2];
-        coord[0] = id % pic.width();
-        coord[1] = (id - (id % pic.width())) / pic.width();
+        coord[0] = id % tempPic.width();
+        coord[1] = (id - (id % tempPic.width())) / tempPic.width();
         return coord;
     }
+    
     public SeamCarver(Picture picture){
         pic = picture;
+        tempPic = new Picture(picture);
         rightSide = true;
         energyGrid = generateEnergy(pic);
+        tempEnergyGrid = energyGrid;
     }
     
     public Picture picture(){
@@ -113,22 +122,23 @@ public class SeamCarver {
     public int[] findHorizontalSeam(){
 //        Only transpose if orientation is wrong.
         if(!rightSide){
-            transposeImage();
+            System.out.println("transposed for horizontal");
+            tempPic = transposeImage(tempPic);
             rightSide = true;
         }
-        int[] results = new int[pic.width()];
-        double[] dist = new double[pic.width() * pic.height()];
-        int[] parentEdge = new int[pic.width() * pic.height()];
+        int[] results = new int[tempPic.width()];
+        double[] dist = new double[tempPic.width() * tempPic.height()];
+        int[] parentEdge = new int[tempPic.width() * tempPic.height()];
         Queue bfsQueue = new Queue();
-        for(int y=0;y<pic.height();y++){
+        for(int y=0;y<tempPic.height();y++){
             int currentV = nodeID(0, y);
             dist[currentV] = 0;   
             parentEdge[currentV] = -1;
             bfsQueue.enqueue(currentV);
 
         }
-        for(int x=1;x<pic.width();x++){
-            for(int y=0;y<pic.height();y++){
+        for(int x=1;x<tempPic.width();x++){
+            for(int y=0;y<tempPic.height();y++){
                 dist[nodeID(x, y)] = Double.MAX_VALUE;
             }
         }
@@ -138,20 +148,20 @@ public class SeamCarver {
             int[] coord = idToCoord(current);
             double currentNodeDist = dist[current];
 
-            if(coord[0] < pic.width()-1){
+            if(coord[0] < tempPic.width()-1){
                 if (coord[1] > 0) {
 //                    Relax top node
                     double oldDist = dist[nodeID(coord[0] + 1, coord[1] - 1)];
-                    double newDist = currentNodeDist + energyGrid[coord[0]+1][coord[1]-1];
+                    double newDist = currentNodeDist + tempEnergyGrid[coord[0]+1][coord[1]-1];
                     if(oldDist > newDist){
                         dist[nodeID(coord[0]+1, coord[1]-1)] = newDist;
                         parentEdge[nodeID(coord[0]+1, coord[1]-1)] = current;
                     }
                     bfsQueue.enqueue(nodeID(coord[0] + 1, coord[1] - 1));
                 }
-                if (coord[1] < pic.height() - 1) {
+                if (coord[1] < tempPic.height() - 1) {
                     double oldDist = dist[nodeID(coord[0] + 1, coord[1] + 1)];
-                    double newDist = currentNodeDist + energyGrid[coord[0] + 1][coord[1] + 1];
+                    double newDist = currentNodeDist + tempEnergyGrid[coord[0] + 1][coord[1] + 1];
                     if (oldDist > newDist) {
                         dist[nodeID(coord[0] + 1, coord[1] + 1)] = newDist;
                         parentEdge[nodeID(coord[0] + 1, coord[1] + 1)] = current;
@@ -159,7 +169,7 @@ public class SeamCarver {
                     bfsQueue.enqueue(nodeID(coord[0] + 1, coord[1] + 1));
                 }
                 double oldDist = dist[nodeID(coord[0] + 1, coord[1])];
-                double newDist = currentNodeDist + energyGrid[coord[0] + 1][coord[1]];
+                double newDist = currentNodeDist + tempEnergyGrid[coord[0] + 1][coord[1]];
                 if (oldDist > newDist) {
                     dist[nodeID(coord[0] + 1, coord[1])] = newDist;
                     parentEdge[nodeID(coord[0] + 1, coord[1])] = current;
@@ -170,8 +180,8 @@ public class SeamCarver {
         int maxNode = 0;
         double maxDist = Double.MAX_VALUE;
         
-        for(int i=0;i<pic.height();i++){
-            int node = nodeID(pic.width()-1, i);
+        for(int i=0;i<tempPic.height();i++){
+            int node = nodeID(tempPic.width()-1, i);
             if(dist[node] < maxDist){
                 maxDist = dist[node];
                 maxNode = node;
@@ -194,15 +204,17 @@ public class SeamCarver {
     
     public int[] findVerticalSeam(){
         if(rightSide){
-            transposeImage();
+            tempPic = transposeImage(tempPic);
         }
         int[] horizontalSeam = findHorizontalSeam();
         rightSide = false;
-        int[] verticalSeam = new int[pic.width()];
+        int[] verticalSeam = new int[pic.height()];
         
         for(int i=0;i<horizontalSeam.length;i++){
-            System.out.println(horizontalSeam[i]);
+            System.out.print(horizontalSeam[i]);
+            verticalSeam[verticalSeam.length-1-i] = horizontalSeam[i];
         }
+
         return verticalSeam;
     }
     
